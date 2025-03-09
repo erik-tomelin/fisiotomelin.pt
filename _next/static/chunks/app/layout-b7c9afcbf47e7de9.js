b0d598bc2d3ea22d5438a98df038dc097380a963
erik_tomelin <erik.tomelin@gesplan.com.br>
2025-03-09 00:34:56 -0300

    Updates

---
+++ _next/static/chunks/app/layout-b7c9afcbf47e7de9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{7383:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,9324,23)),Promise.resolve().then(l.t.bind(l,9840,23))},9324:()=>{},9840:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_3ee9c5"}}},e=>{var s=s=>e(e.s=s);e.O(0,[385,441,517,358],()=>s(7383)),_N_E=e.O()}]);
